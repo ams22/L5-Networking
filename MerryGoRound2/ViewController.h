c637278c19fd07f17667f8c466597d9ddccb76e3
Nikolay Morev <n.morev@corp.mail.ru>
2016-03-23 19:18:04 +0300

    Initial Commit

---
+++ MerryGoRound2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MerryGoRound2
+//
+//  Created by Nikolay Morev on 23/03/16.
+//  Copyright © 2016 Nikolay Morev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MerryGoRound2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MerryGoRound2
+//
+//  Created by Nikolay Morev on 23/03/16.
+//  Copyright © 2016 Nikolay Morev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
